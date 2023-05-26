30c2a0e9e90732aed985dfba0b60848867fc2ba7
rzotr2 <pavel.lukin126@gmail.com>
2023-05-26 14:21:20 +0300

    Added modal window:
    - added Dialog window, added some CSS styles

--- src/JS/createListItem.js
+++ src/JS/createListItem.js
@@ -1,5 +1,6 @@
 import * as restService from "./restService.js";
 import * as loader from './loader.js';
+import * as dialog from './dialog.js';
 
 const listNew = document.querySelector('#todoListNew');
 const listCompleted = document.querySelector('#todoListCompleted');
@@ -15,9 +16,12 @@ const createListItem = (todo) => {
             <label for="input-checkbox" class="todo-list__text tooltip js-title" contenteditable="true">
                 ${todo.title}
             </label>
-            <span class="tooltipText">${todo.title}</span>
+            <span class="tooltipText">${todo.description}</span>
         </div>
         <div class="todo-list__button">
+            <button class="todo-list__edit-button js-edit-button">
+                <span class="todo-list__symbol-edit">&#128393;</span>
+            </button>
             <button class="todo-list__delete-button js-delete-button">
                 <span class="todo-list__symbol-delete">&#x2716;</span>
             </button>
@@ -47,24 +51,19 @@ const addListeners = (listItem, todo) => {
     const deleteButton = listItem.querySelector('.js-delete-button');
     const title = listItem.querySelector('.js-title');
     const tooltipText = listItem.querySelector('.tooltipText');
+    const editButton = listItem.querySelector('.js-edit-button');
 
     checkbox.addEventListener('change', (event) => {
         loader.addLoader();
         todo.completed = event.target.checked;
-        // if (todo.completed) {
-        //     restService.updateTodo(todo).then(() => {
-        //         listItem.remove();
-        //         listCompleted.appendChild(listItem);
-        //     });
-        // } else {
-        //     restService.updateTodo(todo).then(() => {
-        //         listItem.remove();
-        //         listNew.appendChild(listItem);
-        //     });
-        // }
 
         const currentList = todo.completed ? listCompleted : listNew;
+
         updateTodoList(todo, currentList, listItem);
+    });
+
+    editButton.addEventListener('click', () => {
+        dialog.createDialog(todo, title, tooltipText);
     });
 
     deleteButton.addEventListener('click', () => {
@@ -76,11 +75,11 @@ const addListeners = (listItem, todo) => {
             todo.title = event.target.textContent;
 
             restService.updateTodo(todo).then(() => {
-                tooltipText.textContent = todo.title;
+                tooltipText.textContent = todo.description;
             });
         } else {
             event.target.textContent = todo.title;
-            tooltipText.textContent = todo.title;
+            tooltipText.textContent = todo.description;
         }
     });
 };
